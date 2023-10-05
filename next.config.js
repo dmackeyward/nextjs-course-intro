7125a78d6e8d554ff36e630ebbe7525291cb9a31
unknown <biggie9925@hotmail.com>
2023-10-05 15:37:40 +1300

    updated config file

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+// config.js
+
+let MONGODB_URI;
+
+if (process.env.NODE_ENV === 'production') {
+  // For production, use the environment variable
+  MONGODB_URI = process.env.MONGODB_URI;
+} else {
+  // For development, use the local .env variable
+  const { MONGODB_URI: localMongoUri } = process.env;
+  MONGODB_URI = localMongoUri;
+}
+
 module.exports = {
-    
+  MONGODB_URI,
 };
